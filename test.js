3391431e49f0c03b194c27d12160e89e9f7fbedf
Empower <empower@empowersc.com>
2017-07-10 21:59:36 -0400

    Remove API key

--- test.js
+++ test.js
@@ -2,37 +2,19 @@
 
 var CodeCampModule = require('./class');
 
-var testMe = 'this is a test string';
-
-// const readline = require('readline');
-
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-// });
-
-// rl.question('Enter your data: ',
-// (answer) => {
-//     console.log(`Thank you for your valuable feedback: ${answer}`);
-
-//     rl.close();
-// }
-// );
-
-console.log('Hello world!');
-console.log('Value of testMe = ' + testMe);
-
 var SlackBot = require('slackbots');
 
 // create a bot 
 var bot = new SlackBot({
-    token: 'xoxb-210061661409-i8fa5rYctbjxTwbhouKcidLL', // Add a bot https://my.slack.com/services/new/bot and put the token  
+    token: '', // Add a bot https://my.slack.com/services/new/bot and put the token  
     name: 'Slack Bot 1',
 });
 
 CodeCampModule.registerBot(bot);
 
-bot.on('message', CodeCampModule.messageCallback);
+bot.on('message', function(data) {
+    data.
+});
 
 bot.on('start', function() {
     bot.postMessageToChannel('general', 'Slack Bot 1 Reporting...');
